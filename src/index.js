e86b5da4a775d64d61b2d29599f7088706983290
Jordan Jones <proxima.aust@gmail.com>
2019-05-30 16:23:25 -0500

    Refactored unique ID to one file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
+const fs = require('fs')
+var path = require('path')
+const { v4 } = require('uuid')
+const { promisify } = require('util')
+const fileExists = promisify(fs.exists)
+const readFile = promisify(fs.readFile)
+const mkDir = promisify(fs.mkdir)
+const writeFile = promisify(fs.writeFile)
 const { getAllData } = require('systeminformation')
 const app = require('express')()
 
-const { identify } = require('../src/identify');
+const identify = async () => {
+  const homedir = require('os').homedir()
+  const filePath = path.normalize(homedir + '/.system-server/.uuid')
+  try {
+    const exists = await fileExists(filePath)
+
+    if (exists) {
+      const file = await readFile(filePath)
+      return file.toString()
+    } else {
+      var id = v4()
+      await mkDir(homedir + '/.system-server/')
+      await writeFile(filePath, id)
+      return id
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 app.use('/', async (req, res) => {
-  const uniqueID = await identify();
+  const uniqueID = await identify()
   try {
-    const results = await getAllData();
+    const results = await getAllData()
     res.json({
-      _id: uniqueID,
+      id: uniqueID,
       timestamp: Math.floor(Date.now() / 1000),
       ...results
     })
@@ -16,6 +42,5 @@ app.use('/', async (req, res) => {
     res.end(error)
   }
 })
-
 
 export default app
