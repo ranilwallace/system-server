a88682f5512404d7abe0b203dd1d4ee957f5bbe6
Ranil Wallace <ranilwallace@gmail.com>
2019-06-07 15:11:27 -0500

    Moved schemas to a separate file

--- src/index.js
+++ src/index.js
@@ -1,116 +1,54 @@
 const { getAllData } = require('systeminformation')
 const app = require('express')()
+const { v4 } = require('uuid')
 
-//const { typeDefs, resolvers } = require('./gqlConfigTBD');
-const { indentify } = require('../src/indentify');
-
-const { ApolloServer, gql } = require('apollo-server-express');
-
+const { typeDefs } = require('../src/graphql/typeDefs');
+import { GraphQLScalarType } from 'graphql';
+const { ApolloServer } = require('apollo-server-express');
 
 var results = [];
-const uniqueID = indentify();
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    base: Information
-  }
-
-  type Information {
-    _id: String,
-    version: String,
-    system: System,
-    bios: Bios,
-    baseboard: Baseboard,
-    os: OS,
-    versions: Versions,
-    node: String,
-    v8: String,
-    inetLatency: Float
-  }
-
-  type System{
-    manufacturer: String,
-    model: String,
-    version: Float,
-    serial: String,
-    uuid: String,
-    sku: String
-  }
-
-  type Bios {
-    vendor: String,
-    version: String,
-    releaseDate: String,
-    revision: String
-  }
-
-  type Baseboard {
-    manufacturer: String,
-    model: String,
-    version: Float,
-    serial: String,
-    assetTag: String
-  }
-
-  type OS {
-    platform: String,
-    distro: String,
-    release: String,
-    codename: String,
-    kernel: String,
-    arch: String,
-    hostname: String,
-    logofile: String
-  }
-
-  type Versions{
-    kernel: String,
-    openssl: String,
-    node: String,
-    v8: String,
-    npm: String,
-    yarn: String,
-    pm2: String,
-    gulp: String,
-    grunt: String,
-    git: String,
-    tsc: String,
-    mysql: String,
-    redis: String,
-    mongodb: String,
-    nginx: String,
-    php: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    base: () => {
-      return {_id: uniqueID, ...results};
+    systemInformation: () => {
+      return {_id: v4(), ...results};
     },
   },
+  IntToString: new GraphQLScalarType({
+    name: 'IntToString',
+    description: 'Custom scalar type that converts an Int more than 32 bits to a String',
+    parseValue(value) {
+      return value.toString();
+    },
+    serialize(value) {
+      return value.toString();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return ast.value.toString();
+      }
+      return null;
+    },
+  }),
 };
 
-
-function getSysInfo(){
-  getAllData().then((value) => {
-    results = value;
-  }, (reason) => {
-    results = [];
-    console.log(reason);
-  });
+const getResultsAsync = async () => {
+  results = await getAllData();
 }
+getResultsAsync();
+
+
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
-getSysInfo();
+
+
 
 app.use('/', async (req, res) => {
+  const results = await getAllData();
   try {
     res.json({
-      _id: uniqueID,
+      _id: v4(),
       timestamp: Math.floor(Date.now() / 1000),
       ...results
     })
