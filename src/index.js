e0ee1db8560072d1a0b12e176afc79faa9fda686
Ranil Wallace <ranilwallace@gmail.com>
2019-09-05 19:22:36 -0500

    Fixed bugs

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { promisify } = require('util')
 const fileExists = promisify(fs.exists)
 const readFile = promisify(fs.readFile)
 const mkDir = promisify(fs.mkdir)
+var https = require('https')
 const writeFile = promisify(fs.writeFile)
 const { getAllData } = require('systeminformation')
 const app = require('express')()
@@ -29,38 +30,9 @@ const identify = async () => {
   }
 }
 
-const fs = require('fs');
-
 var options = {
   key: fs.readFileSync(__dirname + '/../ssl/server.key'),
   cert: fs.readFileSync(__dirname + '/../ssl/server.cert')
-}
-
-const fs = require('fs');
-
-var options = {
-  key: fs.readFileSync(__dirname + '/../ssl/server.key'),
-  cert: fs.readFileSync(__dirname + '/../ssl/server.cert')
-}
-
-const identify = async () => {
-  const homedir = require('os').homedir()
-  const filePath = path.normalize(homedir + '/.system-server/.uuid')
-  try {
-    const exists = await fileExists(filePath)
-
-    if (exists) {
-      const file = await readFile(filePath)
-      return file.toString()
-    } else {
-      var id = v4()
-      await mkDir(homedir + '/.system-server/')
-      await writeFile(filePath, id)
-      return id
-    }
-  } catch (error) {
-    console.error(error)
-  }
 }
 
 app.use('/', async (req, res) => {
